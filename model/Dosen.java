45cb7bf05c217b6eaf7ecd5bd5a08a6523ea424e
nurdinah <dinah.saja.bjb@gmail.com>
2020-03-26 11:34:14 +0700

    Buat File Input Output

---
+++ model/Dosen.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Dosen{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Dosen ===============");
+  System.out.println("NIDN : 1107057843");
+  System.out.println("Nama : Nurdinah");
+  System.out.println("=============================");
+ }
+}
---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa =================");
+  System.out.println("NPM : 18630412");
+  System.out.println("Nama : Nurdinah");
+  System.out.println("===================================");
+ }
+}
